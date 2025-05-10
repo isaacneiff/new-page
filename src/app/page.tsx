812f460b054cb11f059df06227970edd02e8a8a3
Isaac Neiff <isaacneiff254@gmail.com>
2025-05-10 22:21:41 +0000

    ainda tem seções não alinhadas com o azul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import ContactSection from '@/components/sections/contact-section';
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header academyNameColor="#c5302c" />
+      <Header />
       <main className="flex-grow">
         <HeroSection />
         <AboutSection />
